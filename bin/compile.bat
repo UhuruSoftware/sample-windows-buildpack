772daf5a59e557a71aeb7cec0c11c50fb428c517
vlad.iovanov <vlad.iovanov@uhurusoftware.com>
2013-11-04 17:18:26 -0800

    Fixed copy directory path in compile script. Added logging to the start script.
    Change-Id: I575a4fa7982e47ac4f66636c7670a481204693c3

--- bin/compile.bat
+++ bin/compile.bat
@@ -3,6 +3,7 @@ set build_path=%1
 set cache_path=%2
 :: do whatever is needed for the application to run
 echo Copying buildpack scripts ...
-xcopy %~dp0\..\scheduler %build_path% /S /E
+mkdir %build_path%\scheduler
+xcopy %~dp0\..\scheduler %build_path%\scheduler\ /E
 echo Done.
 exit /b 0
--- scheduler/start.bat
+++ scheduler/start.bat
@@ -1,9 +1,15 @@
-:: PORT
-:: VCAP_WINDOWS_USER
-:: VCAP_WINDOWS_USER_PASSWORD
-:: TEMP
+@echo off
+
+set logfile=%~dp0\..\..\logs\stdout.log
+set errorlog=%~dp0\..\..\logs\stderr.log
+
+echo "Setting up proper directory in the schedule xml file." >> %logfile%
 
 Get-Content /xml %~dp0\..\schedule.xml | ForEach-Object { $_ -replace "^<Command^>", "^<Command^>%HOME%\" } | Set-Content /xml %~dp0\..\%VCAP_WINDOWS_USER%.xml
 Get-Content /xml %~dp0\..\%VCAP_WINDOWS_USER%.xml | ForEach-Object { $_ -replace "^</Command^>", "^</Command^>^<WorkingDirectory^>%HOME%^</WorkingDirectory>^" } | Set-Content /xml %~dp0\..\%VCAP_WINDOWS_USER%.xml
 
-schtasks /create /ru %VCAP_WINDOWS_USER% /rp %VCAP_WINDOWS_USER_PASSWORD% /xml %~dp0\..\%VCAP_WINDOWS_USER%.xml
+echo "Configuring the scheduled task." >> %logfile%
+
+schtasks /create /ru %VCAP_WINDOWS_USER% /rp %VCAP_WINDOWS_USER_PASSWORD% /xml %~dp0\..\%VCAP_WINDOWS_USER%.xml >> %logfile% 2>> %errorlog%
+
+timeout -1
